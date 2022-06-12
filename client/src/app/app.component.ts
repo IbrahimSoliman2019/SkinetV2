81a8db5c0cca5badca50169a72c65ba82ba8d235
Ibrahimsoliman200 <engibrahimsoliman2019@gmail.com>
2022-06-12 20:21:45 +0200

    Angular Basics

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { IPagination } from './models/pagination';
+import { IProduct } from './models/product';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  Products:IProduct[]=[]
+  constructor(private http:HttpClient){}
+  ngOnInit(): void {
+   this.http.get('https://localhost:5001/api/products/').subscribe((response:any)=>{
+     this.Products = response.data     
+   },error=>{
+     console.log(error);
+   })
+  }
+
   title = 'Skinet';
 }
